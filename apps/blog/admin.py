7cdc0cfabe73b3206b9dac85c9f01b8f5a192fcf
sobirjonovme <huntsman6004@gmail.com>
2023-03-15 16:35:30 +0500

    ModelAdmins added

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -1,9 +1,29 @@
 from django.contrib import admin
 
-from .models import Post, PostCategory, PostView, Interview
+from apps.blog.models import Post, PostCategory, PostView, Interview
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'category', 'author', 'view_count')
+    list_filter = ('category', )
+    search_fields = ('title', )
+
+
+class PostCategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name',)
+    search_fields = ('name', )
+
+
+class PostViewAdmin(admin.ModelAdmin):
+    list_display = ('id', 'post', 'user', 'device_id', )
+
+
+class InterviewAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'author', )
+
 
 # Register your models here.
-admin.site.register(Post)
-admin.site.register(PostView)
-admin.site.register(PostCategory)
-admin.site.register(Interview)
+admin.site.register(Post, PostAdmin)
+admin.site.register(PostCategory, PostCategoryAdmin)
+admin.site.register(PostView, PostViewAdmin)
+admin.site.register(Interview, InterviewAdmin)
