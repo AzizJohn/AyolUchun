465938a88b371ee79c2938461abff9bbf654875b
AzizJohn <azizjon1708@gmail.com>
2023-03-15 16:56:27 +0500

    added some features to admin

--- apps/course/admin.py
+++ apps/course/admin.py
@@ -1,14 +1,99 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 from apps.common.models import Author
 from .models import Category, SocialMedia, Course, CourseComment, Section, Lecture, LectureViewed, LectureComment, \
     Certificate, CourseUser
 
-admin.site.register(Author)
-admin.site.register(Category)
-admin.site.register(SocialMedia)
-admin.site.register(Course)
-admin.site.register(CourseUser)
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'gender', 'get_html_photo', 'email')
+    list_display_links = ('id', 'first_name')
+    search_fields = ('first_name', 'last_name')
+    # list_editable = ('is_published',)
+    list_filter = ('gender',)
+    # prepopulated_fields = {"slug": ("firs_name",)}
+    fields = (
+        'gender', 'first_name', 'last_name', 'email', 'profile_pic', 'get_html_photo', 'bio', 'created_at',
+        'updated_at')
+    readonly_fields = ('created_at', 'updated_at', 'get_html_photo')
+    save_on_top = True
+
+    def get_html_photo(self, object):
+        if object.profile_pic:
+            return mark_safe(f"<img src='{object.profile_pic.url}' width=50>")
+
+    get_html_photo.short_description = "Avatar_photo"
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'icon')
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)
+    # list_editable = ('is_published',)
+    # list_filter = ('gender',)
+    # prepopulated_fields = {"slug": ("name",)}
+    fields = (
+        'name', 'icon', 'get_html_photo', 'created_at', 'updated_at')
+    readonly_fields = ('created_at', 'updated_at', 'get_html_photo')
+    save_on_top = True
+
+    def get_html_photo(self, object):
+        if object.icon:
+            return mark_safe(f"<img src='{object.icon.url}' width=50>")
+
+    get_html_photo.short_description = "Category_icon"
+
+
+class SocialMediaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'get_html_photo', 'link', 'redirects', 'order')
+    list_display_links = ('id', 'name',)
+    search_fields = ('name',)
+    # prepopulated_fields = {"slug": ("name",)}
+    fields = (
+        'name', 'icon', 'link', 'redirects', 'order', 'get_html_photo', 'created_at', 'updated_at')
+    readonly_fields = ('created_at', 'updated_at', 'get_html_photo')
+    save_on_top = True
+
+    def get_html_photo(self, object):
+        if object.icon:
+            return mark_safe(f"<img src='{object.icon.url}' width=50>")
+
+    get_html_photo.short_description = "Social Media icon"
+
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'author', 'original_price', 'discounted_price', 'discount_expire_date', 'badge_id')
+    list_display_links = ('id', 'title', 'author')
+    search_fields = ('title', 'author')
+    # list_editable = ('is_published',)
+    # list_filter = ('gender',)
+    #prepopulated_fields = {"slug": ("title",)}
+    fields = (
+        'title', 'badge_id', 'author', 'category_id', 'original_price', 'discounted_price', 'discount_expire_date',
+        'created_at', 'updated_at')
+    readonly_fields = ('created_at', 'updated_at')
+    save_on_top = True
+
+
+class CourseUserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'course', 'user')
+    list_display_links = ('id', 'course', 'user')
+    search_fields = ('course',)
+    # list_editable = ('is_published',)
+    # list_filter = ('gender',)
+    # prepopulated_fields = {"slug": ("name",)}
+    fields = (
+        'user', 'created_at', 'updated_at')
+    readonly_fields = ('created_at', 'updated_at')
+    save_on_top = True
+
+
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(SocialMedia, SocialMediaAdmin)
+admin.site.register(Course, CourseAdmin)
+admin.site.register(CourseUser, CourseUserAdmin)
 admin.site.register(CourseComment)
 admin.site.register(Section)
 admin.site.register(Lecture)
--- apps/course/views.py
+++ apps/course/views.py
@@ -3,84 +3,92 @@ from rest_framework.response import Response
 
 from .certificate_generator import certificate_generate
 from .models import *
-from .permissions import IsAdminOrCreateOnly
+# from .permissions import IsAdminOrCreateOnly
 from .serializers import CategorySerializer, SocialMediaSerializer, CourseSerializer, CourseCommentSerializer, \
     SectionSerializer, LectureSerializer, LectureCommentSerializer, CertificateSerializer, LectureViewedSerializer
+from ..users.api.permissions import IsAuthDone
 
 
 class CategoryList(ListAPIView):
-    permission_classes = (IsAdminOrCreateOnly,)
+    permission_classes = (IsAuthDone,)
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
 
 class CategoryDetailAPIView(RetrieveAPIView):
+    permission_classes = (IsAuthDone,)
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
 
 class SocialMediaList(ListAPIView):
-    permission_classes = (IsAdminOrCreateOnly,)
+    permission_classes = (IsAuthDone,)
     queryset = SocialMedia.objects.all()
     serializer_class = SocialMediaSerializer
 
 
 class SocialMediaDetailAPIView(RetrieveAPIView):
+    permission_classes = (IsAuthDone,)
     queryset = SocialMedia.objects.all()
     serializer_class = SocialMediaSerializer
 
 
 class CourseList(ListAPIView):
-    permission_classes = (IsAdminOrCreateOnly,)
+    permission_classes = (IsAuthDone,)
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
 
 
 class CourseDetailAPIView(RetrieveAPIView):
+    permission_classes = (IsAuthDone,)
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
 
 
 class CourseCommentList(ListAPIView):
-    permission_classes = (IsAdminOrCreateOnly,)
+    permission_classes = (IsAuthDone,)
     queryset = CourseComment.objects.all()
     serializer_class = CourseCommentSerializer
 
 
 class CourseCommentDetailAPIView(RetrieveAPIView):
+    permission_classes = (IsAuthDone,)
     queryset = CourseComment.objects.all()
     serializer_class = CourseCommentSerializer
 
 
 class SectionList(ListAPIView):
-    permission_classes = (IsAdminOrCreateOnly,)
+    permission_classes = (IsAuthDone,)
     queryset = Section.objects.all()
     serializer_class = SectionSerializer
 
 
 class SectionDetailAPIView(RetrieveAPIView):
+    permission_classes = (IsAuthDone,)
     queryset = Section.objects.all()
     serializer_class = SectionSerializer
 
 
 class LectureList(ListAPIView):
-    permission_classes = (IsAdminOrCreateOnly,)
+    permission_classes = (IsAuthDone,)
     queryset = Lecture.objects.all()
     serializer_class = LectureSerializer
 
 
 class LectureDetailAPIView(RetrieveAPIView):
+    permission_classes = (IsAuthDone,)
     queryset = Lecture.objects.all()
     serializer_class = LectureSerializer
 
 
 class LectureViewedList(ListAPIView):
-    permission_classes = (IsAdminOrCreateOnly,)
+    permission_classes = (IsAuthDone,)
     queryset = LectureViewed.objects.all()
     serializer_class = LectureViewedSerializer
 
 
 class LectureViewedDetailAPIView(RetrieveAPIView):
+    permission_classes = (IsAuthDone,)
     queryset = LectureViewed.objects.all()
     serializer_class = LectureViewedSerializer
 
@@ -91,23 +99,25 @@ class LectureViewedDetailAPIView(RetrieveAPIView):
 
 
 class LectureCommentList(ListAPIView):
-    permission_classes = (IsAdminOrCreateOnly,)
+    permission_classes = (IsAuthDone,)
     queryset = LectureComment.objects.all()
     serializer_class = LectureCommentSerializer
 
 
 class LectureCommentDetailAPIView(RetrieveAPIView):
+    permission_classes = (IsAuthDone,)
     queryset = LectureComment.objects.all()
     serializer_class = LectureCommentSerializer
 
 
 class CertificateList(ListAPIView):
-    permission_classes = (IsAdminOrCreateOnly,)
+    permission_classes = (IsAuthDone,)
     queryset = Certificate.objects.all()
     serializer_class = CertificateSerializer
 
 
 class CertificateDetailAPIView(RetrieveAPIView):
+    permission_classes = (IsAuthDone,)
     queryset = Certificate.objects.all()
     serializer_class = CertificateSerializer
 
@@ -124,6 +134,7 @@ class CertificateDetailAPIView(RetrieveAPIView):
 #     return Response(serizalizer)
 
 class CreateCertificate(CreateAPIView):
+    permission_classes = (IsAuthDone,)
     queryset = Certificate.objects.all()
     serializer_class = CertificateSerializer
 
